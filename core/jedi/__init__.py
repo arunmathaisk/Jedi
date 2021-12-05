0b6abd840ae663a8c3d2dcfa5426fc0274c6f69e
Arun <me@arunmathaisk.in>
2021-12-05 10:11:47 +0530

    dbschema

--- core/jedi/__init__.py
+++ core/jedi/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'dkljsakljdlksajlk'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jedi.db'
+db = SQLAlchemy(app)
 from jedi.routes import home
---
+++ core/jedi/modals/dbschema.py
@@ -0,0 +1,15 @@
+from jedi import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(15),nullable=False,unique=True)
+    email = db.Column(db.String(50),nullable=False,unique=True)
+    password = db.Column(db.String(80),nullable=False)   
+    posts = db.relationship('BPost',backref='author',lazy=True)
+
+
+class BPost(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    tweet = db.Column(db.String(500),nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
