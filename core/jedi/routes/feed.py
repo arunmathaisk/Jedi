25cce7f2165466741436d8f25341f35259dfe25a
Arun <me@arunmathaisk.in>
2022-04-29 00:14:46 +0530

    boosted post added in tweet

--- core/jedi/routes/feed.py
+++ core/jedi/routes/feed.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, session, redirect
 from jedi import app
-from jedi.modals.dbschema import db, User, Post
+from jedi.modals.dbschema import db, User, Post, Boostpost
 import json
 import datetime
 
@@ -53,3 +53,29 @@ def created_posts():
             tmp_dict.update({"id":i.id})
             posts_list.append(tmp_dict)
         return json.dumps(posts_list)
+
+
+@app.post('/allboostedposts')
+def allboostedposts():
+    if not 'uid' in session:
+        res_obj = {}
+        res_obj.update({"status": 1})
+        res_obj.update({"error": 'not logged in'})
+        return json.dumps(res_obj)
+    else:
+        boostedposts = Boostpost.query.all()
+        boostedposts_list = []
+        for i in boostedposts:
+            post  =  Post.query.get(i.og_post_id)
+            og_user = User.query.get(post.user_id)
+            boosted_user = User.query.get(i.user_id)
+            tmp_dict = {}
+            tmp_dict.update({"boostpost_id":i.id})
+            tmp_dict.update({"og_post_id":i.og_post_id})
+            tmp_dict.update({"og_post_username":og_user.username})
+            tmp_dict.update({"boosteduser_username":boosted_user.username})
+            tmp_dict.update({"og_post_content":post.content})
+            tmp_dict.update({"og_content_hash":post.content_hash})
+            tmp_dict.update({"timestamp":i.timestamp})
+            boostedposts_list.append(tmp_dict)
+        return json.dumps(boostedposts_list)
